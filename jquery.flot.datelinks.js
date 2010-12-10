a98217b1375b7db7f0e7651f5fdd0827aeafee92
Eric Wendelin <eric.wendelin@returnpath.net>
2010-12-10 15:17:24 -0700

    Now shows selected time range

--- jquery.flot.datelinks.js
+++ jquery.flot.datelinks.js
@@ -23,6 +23,15 @@ users to select different time ranges (e.g. max, 1y, 6m, 3m, 1m, 2w, 1w).
     };
     
     function init(plot) {
+		var dayInMillis = 1000 * 60 * 60 * 24,
+			monthInMillis = 30 * dayInMillis;
+
+		// NOTE: must be iterated in sorted order
+		// FIXME: this a stupid way to do months, but we'll care about that later
+		var timePeriods = { '1w': 7 * dayInMillis, '2w': 14 * dayInMillis,
+				'1m': monthInMillis, '3m': 3 * monthInMillis, '6m': 6 * monthInMillis,
+				'1y': 12 * monthInMillis };
+	
 		/**
 		 * Inject links into the top-left of a graph for different time ranges.
 		 *
@@ -37,46 +46,24 @@ users to select different time ranges (e.g. max, 1y, 6m, 3m, 1m, 2w, 1w).
 				mintime = xaxis.datamin,
 				maxtime = xaxis.datamax,
 				timediff = maxtime - mintime,
-				dayInMillis = 1000 * 60 * 60 * 24,
-				monthInMillis = 30 * dayInMillis;
-
-			// NOTE: must be iterated in sorted order
-			// NOTE: this a stupid way to do months, but we'll care about that later
-			var timePeriods = { '1w': 7 * dayInMillis, '2w': 14 * dayInMillis,
-					'1m': monthInMillis, '3m': 3 * monthInMillis, '6m': 6 * monthInMillis,
-					'1y': 12 * monthInMillis };
-
-			var topLeftOffset = plot.pointOffset({ x: xaxis.min, y: plot.getAxes().yaxis.max });
-			var fragment = document.createDocumentFragment();
-			var wrapper = document.createElement('div');
+			    selectedTimePeriod = plot.getPlaceholder().attr('time-period') || 'max',
+			    topLeftOffset = plot.pointOffset({ x: xaxis.min, y: plot.getAxes().yaxis.max }),
+			    fragment = document.createDocumentFragment(),
+			    wrapper = document.createElement('div');
+			
 			wrapper.id = 'date-period-links';
+
 			// TODO: this puts it in the top-left, but there's no reason we can't move this around
 			wrapper.style.cssText = 'position:absolute; left:' + (topLeftOffset.left + 5) + 'px; top: 10px;';
 			var link = document.createElement('span');
-			link.style.cssText = 'color: #00F; text-decoration: underline; font-size: 0.85em; padding-right: 5px; cursor: pointer;';
 			link.innerHTML = 'max';
-			
-			var replot = function(plot, xmin, xmax) {
-				// Return an onclick event handler function
-				return function(evt) {
-					var data = plot.getData();
-					// Keep all options except axes limits
-					for (var i = 0, len = data.length; i < len; i++) {
-						// FIXME: need to set yaxis value differently, cannot find a way to detect it from data object
-						// THIS WILL BREAK WHEN TRYING TO USE MULTIPLE DATASETS WITHOUT MULTIPLE Y-AXES!
-						var yaxis = i + 1;
-						delete data[i].xaxis;
-						data[i].yaxis = yaxis;
-						delete data[i].datapoints;
-					}
-					plot = $.plot(plot.getPlaceholder(), data, $.extend(true, {}, opts, {
-						xaxis: { min: xmin, max: xmax },
-						x2axis: { min: xmin, max: xmax }
-					}));
-				}
+			if (selectedTimePeriod !== 'max') {
+				linkifyTimePeriod(link, plot, mintime, maxtime);
+			} else {
+				delinkifyTimePeriod(link);
 			}
+			
 			// Plot with all data
-			link.onclick = replot(plot, mintime, maxtime);
 			wrapper.appendChild(link);
 			
 			for (var timePeriod in timePeriods) {
@@ -85,7 +72,11 @@ users to select different time ranges (e.g. max, 1y, 6m, 3m, 1m, 2w, 1w).
 						// Append link to html fragment
 						var linkClone = link.cloneNode(true);
 						linkClone.innerHTML = timePeriod;
-						linkClone.onclick = replot(plot, maxtime - timePeriods[timePeriod], maxtime);
+						if (selectedTimePeriod !== timePeriod) {
+							linkifyTimePeriod(linkClone, plot, mintime, maxtime);
+						} else {
+							delinkifyTimePeriod(linkClone);
+						}
 						wrapper.appendChild(linkClone);
 					} else { //No more links with greater time periods will be necessary
 						break;
@@ -97,6 +88,44 @@ users to select different time ranges (e.g. max, 1y, 6m, 3m, 1m, 2w, 1w).
 			plot.getPlaceholder().append(fragment);
 		}
 		
+		function linkifyTimePeriod(el, plot, mintime, maxtime) {
+			el.style.cssText = 'color: #00F; text-decoration: underline; font-size: 0.85em; padding-right: 5px; cursor: pointer;';
+			el.onclick = generateReplotEvent(plot, el.innerHTML, mintime, maxtime);
+		}
+		
+		function delinkifyTimePeriod(el) {
+			el.style.cssText = 'color: #000; font-weight: bold; font-size: 0.85em; padding-right: 5px;';
+		}
+		
+		function generateReplotEvent(plot, timePeriod, mintime, maxtime) {
+			// Return an onclick event handler function
+			return function(evt) {
+				var data = plot.getData();
+				// Keep all options except axes limits
+				for (var i = 0, len = data.length; i < len; i++) {
+					// FIXME: need to set yaxis value differently, cannot find a way to detect it from data object
+					// THIS WILL BREAK WHEN TRYING TO USE MULTIPLE DATASETS WITHOUT MULTIPLE Y-AXES!
+					var yaxis = i + 1;
+					delete data[i].xaxis;
+					data[i].yaxis = yaxis;
+					delete data[i].datapoints;
+				}
+				var ph = plot.getPlaceholder();
+				ph.attr('time-period', timePeriod);
+				if (timePeriod === 'max') {
+					plot = $.plot(ph, data, $.extend(true, {}, plot.getOptions(), {
+						xaxis: { min: mintime, max: maxtime },
+						x2axis: { min: mintime, max: maxtime }
+					}));
+				} else {
+					plot = $.plot(ph, data, $.extend(true, {}, plot.getOptions(), {
+						xaxis: { min: maxtime - timePeriods[timePeriod], max: maxtime },
+						x2axis: { min: maxtime - timePeriods[timePeriod], max: maxtime }
+					}));
+				}
+			}
+		}
+		
         plot.hooks.draw.push(createLinks);
     }
     
@@ -104,6 +133,6 @@ users to select different time ranges (e.g. max, 1y, 6m, 3m, 1m, 2w, 1w).
         init: init,
         options: options,
         name: 'datelinks',
-        version: '0.1'
+        version: '0.2'
     });
 })(jQuery);
